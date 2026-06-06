ee4d69c195055ee8abeb8fa006cd047d1ece11de
BluTargaryan <opeyemiyolo@gmail.com>
2026-06-06 14:34:18 +0100

    chore: update HeroButtonSection to navigate to contact and about sections on button click

--- app/components/sections/HeroButtonSection.tsx
+++ app/components/sections/HeroButtonSection.tsx
@@ -3,12 +3,14 @@
 import React from 'react'
 import Button from '../atoms/Button'
 import { MdGroups, MdPersonAdd } from "react-icons/md"
+import { useRouter } from 'next/navigation'
 
 const HeroButtonSection = () => {
+  const router = useRouter()
   return (
 <div className="flex flex-col gap-2 md:flex-row xl:gap-4">
-  <Button text="Contact a Pro" onClick={() => {}} icon={<MdPersonAdd />} className="bg-primary text-background hover:text-text hover:bg-accent hover:border hover:border-text w-fit xl:text-2xl xl:h-14 xl:px-6 xl:gap-4" />
-  <Button text="Who we are" onClick={() => {}} icon={<MdGroups />} className="bg-background text-text border border-text hover:bg-hover w-fit xl:text-2xl xl:h-14 xl:px-6 xl:gap-4" />
+  <Button text="Contact a Pro" onClick={() => {router.push('/#contact')}} icon={<MdPersonAdd />} className="bg-primary text-background hover:text-text hover:bg-accent hover:border hover:border-text w-fit xl:text-2xl xl:h-14 xl:px-6 xl:gap-4" />
+  <Button text="Who we are" onClick={() => {router.push('/#about')}} icon={<MdGroups />} className="bg-background text-text border border-text hover:bg-hover w-fit xl:text-2xl xl:h-14 xl:px-6 xl:gap-4" />
 </div>
   )
 }
